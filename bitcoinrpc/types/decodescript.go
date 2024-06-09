4e2f84f93504dd8d2ddb765ad05bc43ec576685a
Raghav Sood <r@raghavsood.com>
2024-06-09 19:15:00 +0800

    bitcoinrpc: add decodescript

---
+++ bitcoinrpc/types/decodescript.go
@@ -0,0 +1,19 @@
+package types
+
+type DecodeScript struct {
+	Asm     string `json:"asm"`
+	Desc    string `json:"desc"`
+	Type    string `json:"type"`
+	Address string `json:"address"`
+	P2SH    string `json:"p2sh"`
+	Segwit  Segwit `json:"segwit"`
+}
+
+type Segwit struct {
+	Asm        string `json:"asm"`
+	Hex        string `json:"hex"`
+	Type       string `json:"type"`
+	Address    string `json:"address"`
+	Desc       string `json:"desc"`
+	P2SHSegwit string `json:"p2sh-segwit"`
+}
