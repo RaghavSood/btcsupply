080c9026be3797f5d21c2009add932dfa0c6334e
Raghav Sood <r@raghavsood.com>
2024-06-16 02:37:02 +0800

    address: add support for p2sh addresses

--- address/address.go
+++ address/address.go
@@ -25,6 +25,11 @@ func addressBytesToScript(address []byte) ([]byte, error) {
 		script := append([]byte{0x76, 0xa9, 0x14}, address[1:21]...)
 		script = append(script, 0x88, 0xac)
 		return script, nil
+	case 0x05:
+		// P2SH
+		script := append([]byte{0xa9, 0x14}, address[1:21]...)
+		script = append(script, 0x87)
+		return script, nil
 	}
 
 	return []byte{}, fmt.Errorf("Unknown address type: %x", address[0])
